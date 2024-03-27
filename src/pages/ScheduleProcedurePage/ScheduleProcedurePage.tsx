2d40c802933a9b74f333d95809f2882ae2314f61
amanzi-lucasvaz <lucas.vaz@amanzi.io>
2024-03-27 04:24:44 -0300

    feat: agendamentos list pagination

--- src/pages/ScheduleProcedurePage/ScheduleProcedurePage.tsx
+++ src/pages/ScheduleProcedurePage/ScheduleProcedurePage.tsx
@@ -6,6 +6,7 @@ import PageContainer from "../../components/containers/PageContainer";
 import useApi from "../../hooks/useApi";
 import trashCan from "../../assets/trashCan.svg"
 import whatsappIcon from "../../assets/whatsappIcon.svg"
+import rightArrow from "../../assets/rightArrow.svg";
 import ActionButton from "../../components/buttons/ActionButton";
 
 
@@ -41,20 +42,52 @@ const ScheduleProcedurePage = () => {
 
 const AgendamentosDisplay = ( {agendamentos, loadAgendamentos} : 
     {agendamentos: IAgendamentos[], loadAgendamentos: () => Promise<void>}) => {
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const itemsPerPage = 10;
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentAgendamentos = agendamentos.slice(indexOfFirstItem, indexOfLastItem);
+
+    const paginateNext = () => {
+        if (indexOfLastItem < agendamentos.length) {
+            setCurrentPage((prevPage) => prevPage + 1);
+        }
+    };
+    
+    const paginatePrevious = () => {
+        if (currentPage > 1) {
+            setCurrentPage((prevPage) => prevPage - 1);
+        }
+    };
+
     return (
         <section className="border border-red w-full flex flex-col duration-300">
+        <div className="flex justify-between items-end">
             <h2 className="font-semibold text-stc-blue text-[20px] ">Próximos Agendamentos</h2>
-            <AgendamentoHeader/>
-            <div className="flex flex-col mt-4 gap-4">
-                { agendamentos.map( agendamento => (
-                    <Agendamento 
-                        key={agendamento.dataAgendamento} 
-                        agendamento={agendamento} 
-                        loadAgendamentos={loadAgendamentos} 
-                    />
-                ))}
+            <div className="flex justify-center mt-4">
+                <button onClick={paginatePrevious} disabled={currentPage === 1}
+                    className="bg-stc-blue mr-2  disabled:bg-gray-100 hover:brightness-90 px-4 py-2 rounded-l"
+                >
+                    <img className="rotate-180" src={rightArrow} alt="" />
+                </button>
+                <button onClick={paginateNext} disabled={indexOfLastItem >= agendamentos.length}
+                    className="bg-stc-blue disabled:bg-gray-100 hover:brightness-90 px-4 py-2 rounded-r"
+                >
+                    <img src={rightArrow} alt="" />
+                </button>
             </div>
-        </section>
+        </div>
+        <AgendamentoHeader/>
+        <div className="flex flex-col mt-4 gap-4">
+            { currentAgendamentos.map( agendamento => (
+                <Agendamento 
+                    key={`${agendamento.agendamentoId}${agendamento.dataAgendamento}`} 
+                    agendamento={agendamento} 
+                    loadAgendamentos={loadAgendamentos} 
+                />
+            ))}
+        </div>
+    </section>
     );
 };
 
